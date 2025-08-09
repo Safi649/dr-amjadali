7811bdf9c3c1939ae480ecb00105c1b33e0f7ea5
Safi649 <muhammadabbassafi332@gmail.com>
2025-08-09 21:32:01 +0500

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,6 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-
+import { analytics } from "@/firebase/firebaseConfig";
 export default function AboutPage() {
   return (
     <>
