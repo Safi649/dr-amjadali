6b1a3ebc4de1e95ced5ccfedc1494d12cd1c559a
Safi649 <muhammadabbassafi332@gmail.com>
2025-08-09 20:12:50 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,13 @@
+import '../styles/globals.css'
+import { AnimatePresence } from "framer-motion";
+
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <AnimatePresence mode="wait" initial={true}>
+      <Component {...pageProps} key={router.route} />
+    </AnimatePresence>
+  )
+}
+
+export default MyApp;
 
