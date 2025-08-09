12b9249dafa3582bde7f931b60d91e72b6127b25
Safi649 <muhammadabbassafi332@gmail.com>
2025-08-09 20:11:25 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0ea5a4', // teal
+        medblue: '#0f172a'
+      }
+    }
+  },
+  plugins: []
+}
 
