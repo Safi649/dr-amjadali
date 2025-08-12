fe9ed9eb58cdd2050b0300a3c1e23b07230ac9d0
Safi649 <muhammadabbassafi332@gmail.com>
2025-08-12 16:56:58 +0500

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,4 @@
-import Header from "../components/Header";
+
 import Footer from "../components/Footer";
 import ContactForm from "../components/ContactForm";
 import MapEmbed from "../components/MapEmbed";
